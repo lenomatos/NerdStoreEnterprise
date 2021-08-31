eb960c222372209f714bf749daec9b1e3b036459
lenomatos <leno_matos@hotmail.com>
2021-08-31 17:59:16 -0300

    Update, continuando estudando

--- src/web/NSE.WebApp.MVC/Configuration/DependecyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependecyInjectionConfig.cs
@@ -2,6 +2,9 @@
 using Microsoft.Extensions.DependencyInjection;
 using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
+using NSE.WebApp.MVC.Services.Handlers;
+using Polly;
+using System;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -9,8 +12,18 @@ namespace NSE.WebApp.MVC.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
+
+            services.AddHttpClient<ICatalogoService, CatalogoService>()
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+                //.AddPolicyHandler(PollyExtensions.EsperarTentar())
+                //.AddTransientHttpErrorPolicy(
+                //    p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
+
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
             services.AddScoped<IUser, AspNetUser>();
         }
     }
