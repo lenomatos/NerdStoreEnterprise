b3cad03528044dec1d76a6aada8fc5b00a2f54a3
lenomatos <leno_matos@hotmail.com>
2021-09-04 00:18:59 -0300

    Updating

--- src/web/NSE.WebApp.MVC/Configuration/DependecyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependecyInjectionConfig.cs
@@ -4,7 +4,10 @@ using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
 using NSE.WebApp.MVC.Services.Handlers;
 using Polly;
+using Polly.Extensions.Http;
+using Polly.Retry;
 using System;
+using System.Net.Http;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -17,14 +20,36 @@ namespace NSE.WebApp.MVC.Configuration
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
 
             services.AddHttpClient<ICatalogoService, CatalogoService>()
-                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
-                //.AddPolicyHandler(PollyExtensions.EsperarTentar())
-                //.AddTransientHttpErrorPolicy(
-                //    p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+                .AddPolicyHandler(PollyExtensions.EsperarTentar())
+                .AddTransientHttpErrorPolicy(
+                    p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
             services.AddScoped<IUser, AspNetUser>();
+        }
+    }
+
+    public class PollyExtensions
+    {
+        public static AsyncRetryPolicy<HttpResponseMessage> EsperarTentar()
+        {
+            var retry = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(10),
+                }, (outcome, timespan, retryCount, context) =>
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Tentando pela {retryCount} vez!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                });
+
+            return retry;
         }
     }
 }
